8671c574651caef38364e78cb15257de0f7d12a8
zaketn <specot70@gmail.com>
2024-12-18 23:20:50 +0500

    implement service layer

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.23.3
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/zaketn/sso-contracts v0.0.1 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
 	google.golang.org/grpc v1.69.0 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
---
+++ internal/services/auth/auth.go
@@ -0,0 +1,143 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/zaketn/sso/internal/domain/models"
+	"github.com/zaketn/sso/internal/lib/jwt"
+	"github.com/zaketn/sso/internal/storage"
+	"golang.org/x/crypto/bcrypt"
+	"log/slog"
+	"time"
+)
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userId int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appId int) (models.App, error)
+}
+
+var (
+	ErrInvalidCredentials = errors.New("the credentials is invalid")
+)
+
+// New returns new instance of the Auth structure
+func New(
+	log *slog.Logger,
+	usrSaver UserSaver,
+	usrProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    usrSaver,
+		userProvider: usrProvider,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+func (a *Auth) Login(ctx context.Context, email, password string, appId int) (token string, err error) {
+	const op = "Auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("username", email),
+	)
+	log.Info("attempting to log user")
+
+	usr, err := a.userProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", err)
+
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+
+		a.log.Error("failed to get user", err)
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(usr.PassHash, []byte(password)); err != nil {
+		a.log.Error("invalid credentials", err)
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appId)
+	if err != nil {
+		a.log.Error("invalid app", err)
+
+		return "", fmt.Errorf("%s: %w", op, storage.ErrAppNowFound)
+	}
+
+	token, err = jwt.NewToken(usr, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed generate token", err)
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
+}
+
+func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (userId int64, err error) {
+	const op = "Auth.RegisterNewUser"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash")
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user")
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, userId int64) (bool, error) {
+	const op = "Auth.IsAdmin"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int64("userId", userId),
+	)
+
+	isAdmin, err := a.userProvider.IsAdmin(ctx, userId)
+	if err != nil {
+		log.Error("failed to get admin info")
+
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
+}
